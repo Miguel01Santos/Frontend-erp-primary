94f9a8d7d6917289c111d2789aa97d5c01173c5c
Miguel dos Santos <migueldossantos.pro@outlook.com>
2024-11-03 15:20:10 -0300

    fix: settings to first page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,13 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+import { LoginPageComponentComponent } from './pages/login-page-component/login-page-component.component';
+import { LicensePurchaseComponentComponent } from './pages/license-purchase-component/license-purchase-component.component';
+
+export const routes: Routes = [
+    {
+        path: '', component: LoginPageComponentComponent
+    },
+    {
+        path: 'routeLicense', component: LicensePurchaseComponentComponent
+    }
+];
--- src/app/pages/login-page-component/login-page-component.component.ts
+++ src/app/pages/login-page-component/login-page-component.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
+import { AccountLoginComponentComponent } from "../../shared/components/account-login-component/account-login-component.component";
+import { FormLoginComponentComponent } from "../../shared/components/form-login-component/form-login-component.component";
 
 @Component({
   selector: 'app-login-page-component',
   standalone: true,
-  imports: [],
+  imports: [AccountLoginComponentComponent, FormLoginComponentComponent],
   templateUrl: './login-page-component.component.html',
   styleUrl: './login-page-component.component.css'
 })
